80387263d7342abdc9fbd3e693bd1d2468b2f951
LeonardoRinaldi92 <leonardo.rinaldi.ph@gmail.com>
2023-07-21 18:35:59 +0200

    struttra home e pckge json

--- src/pages/CompPage_Home.vue
+++ src/pages/CompPage_Home.vue
@@ -7,6 +7,7 @@ import { store } from '../store';
         data()
         {
             return {
+                store
             }
         },
         created()
@@ -17,9 +18,46 @@ import { store } from '../store';
 </script>
 
 <template>
-    <h1>I am the main page</h1>
+    <div v-if="store.apartments !== null">
+        <div class="row container mx-auto">
+            <div v-for="(apartment, index) in store.apartments.data " :key='index' class="p-1 col-4">
+                <div class=" p-2 my-1">
+                    <div class="overflow-hidden rounded-4" style="height: 300px;">
+                        <img :src="`http://127.0.0.1:8000/storage/${apartment.cover_img}`" alt="" class="img-box">
+                    </div>
+                    <div class="mt-3">                        
+                        <p>
+                            <b>
+                                {{ apartment.title }}, 
+                            </b>
+                            <span class="ms-2">
+                                <b>
+                                    <i>
+                                        {{ apartment.city }}
+                                    </i>
+                                </b>
+                            </span>
+                        </p>
+                        <p class="mt-0">
+                            <i>
+                                {{ apartment.address }}
+                            </i>
+                        </p>
+                    </div>
+                    
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <style lang="scss">
+    .img-box {
+    object-fit: cover;
+    object-position: top;
+    width: 100%;
+    height: 100%;
+    display: block;
+}
 
 </style>
