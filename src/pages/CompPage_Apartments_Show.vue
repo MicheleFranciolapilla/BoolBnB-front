b02b68ed2f5333d98745cfd56a7366b9c4b3a4a2
LeonardoRinaldi92 <leonardo.rinaldi.ph@gmail.com>
2023-07-29 11:00:50 +0200

    eoluzione show

--- src/pages/CompPage_Apartments_Show.vue
+++ src/pages/CompPage_Apartments_Show.vue
@@ -125,17 +125,31 @@ import axios from "axios";
 
 <template>
     <div v-if="!store.axios_running" class="container">
-        <div class="d-flex justify-content-between mt-2 align-items-center">
-            <h1>
-                {{ store.one_apartment.title }}
-            </h1>    
-            <div>
-                <div class="text-center">
+        <div class="mt-2 align-items-center">
+            <div class="row mb-2">
+                <div class="col-12 text-center">
+                    <h1>
+                        {{ store.one_apartment.title }}
+                    </h1>
+                    <div>
+                        <i>
+                            {{store.one_apartment.address  }}
+                        </i>
+                        <span class="ms-2">
+                            <b>
+                                {{store.one_apartment.city }}
+                            </b>
+                        </span>
+                    </div>    
+                </div>
+                <div class="col-6 text-end d-none d-md-block">
                     <!-- btn per form msg -->
                     <button class="btn btn-success" type="button" data-bs-toggle="offcanvas" data-bs-target="#staticBackdrop" aria-controls="staticBackdrop">
                     Contatta l'Host!
                     </button>    
                 </div>
+            </div>
+            <div>
                 
 
                 <div class="offcanvas offcanvas-end" data-bs-backdrop="static" tabindex="-1" id="staticBackdrop" aria-labelledby="staticBackdropLabel" style="width: 35vw;">
@@ -147,16 +161,7 @@ import axios from "axios";
         </div>
         
         
-        <p class="mt-3">
-            <i>
-                {{store.one_apartment.address  }}
-            </i>
-            <span class="ms-2">
-                <b>
-                    {{store.one_apartment.city }}
-                </b>
-            </span>
-        </p>
+
         <div class="row">
             <div class="col-6 p-2">
                 <div class="overflow-hidden box-image-sx" style="height: 100%; max-height: 500px;">
