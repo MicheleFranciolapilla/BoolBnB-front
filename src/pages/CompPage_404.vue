bd4076d5fc1233e0697ea37acc2ef268188b1fcc
edoardope <pencoedoardo@gmail.com>
2023-07-29 20:22:38 +0200

    404 rework

--- src/pages/CompPage_404.vue
+++ src/pages/CompPage_404.vue
@@ -1,9 +1,15 @@
 <script>
     import { store } from '../store';
+    import Comp_Header from '../components/Comp_Header.vue';
+    import Comp_Footer from '../components/Comp_Footer.vue';
 
     export default
     {
         name    :   "CompPage_404",
+        components: {
+            Comp_Header,
+            Comp_Footer,
+        },
         data()
         {
             return  {
@@ -19,16 +25,31 @@
 </script>
 
 <template>
-    <h1 class="mt-5 text-center text-danger">ERRORE 404</h1>
-    <h2 class="mt-3 text-center text-warning">{{ store.api_error.error_msg }}</h2>
+<body>
+    <div class="wrapper">
+        <Comp_Header />
+        <h2 class="mt-3 text-center text-warning">{{ store.api_error.error_msg }}</h2>
 
-    <div class="d-flex justify-content-center">
-        <router-link class="nav-link btn btn-primary w-25" aria-current="page" to="/">Torna alla home</router-link>
+        <div class="d-flex justify-content-center">
+            <router-link class="nav-link btn btn-primary w-25" aria-current="page" to="/">Torna alla home</router-link>
+        </div>
+
     </div>
-
+    <Comp_Footer />
+</body>
 </template>
 
 <style lang="scss">
+body{
+    margin: 0;
+    padding: 0;
+    min-height: 100vh; 
+    display: flex;
+    flex-direction: column;
+}
 
+.wrapper {
+    flex: 1; /* Il contenuto principale (wrapper) occupa lo spazio disponibile rimanente */
+}
 
 </style>
